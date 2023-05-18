c7cd0190f5c4e33c423cbe9a8ea9da98b1188110
Hasnathasan <hasnatoooooooo@gmail.com>
2023-05-18 22:00:34 +0600

    router setup successfully

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: ture },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
