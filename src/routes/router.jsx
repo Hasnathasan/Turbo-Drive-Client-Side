7a46aea4508791a9bb4ea48e7e5852dda24c2e69
Hasnathasan <hasnatoooooooo@gmail.com>
2024-01-31 23:34:19 +0600

    Code changes in router

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,70 +1,91 @@
-import {
-    createBrowserRouter,
-  } from "react-router-dom";
-import Main from '../Pages/Main/Main';
-import Home from '../Pages/HomePage/Home/Home';
-import Login from '../Pages/LoginPage/Login';
-import SignUp from '../Pages/SignupPage/SignUp';
-import AuthProvider from '../Provider/AuthProvider';
-import AllToy from '../Pages/AllToys/AllToy';
-import AddToy from '../Pages/AddToy/AddToy';
-import Blog from '../Pages/Blog/Blog';
-import MyToys from '../Pages/MyToys/MyToys';
-import EditToy from '../Pages/MyToys/EditToy';
-import Details from '../Pages/Details/Details';
-import ErrorPage from '../Pages/ErrorPage/ErrorPage';
-import PrivateRoute from '../Pages/PrivateRoute/PrivateRoute';
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Pages/Main/Main";
+import Home from "../Pages/HomePage/Home/Home";
+import Login from "../Pages/LoginPage/Login";
+import SignUp from "../Pages/SignupPage/SignUp";
+import AuthProvider from "../Provider/AuthProvider";
+import AllToy from "../Pages/AllToys/AllToy";
+import AddToy from "../Pages/AddToy/AddToy";
+import Blog from "../Pages/Blog/Blog";
+import MyToys from "../Pages/MyToys/MyToys";
+import EditToy from "../Pages/MyToys/EditToy";
+import Details from "../Pages/Details/Details";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import PrivateRoute from "../Pages/PrivateRoute/PrivateRoute";
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <AuthProvider><Main></Main></AuthProvider>,
-      children: [
-        {
-            path: '/',
-            element: <Home></Home>
-        },
-        {
-            path: '/login',
-            element: <Login></Login>
-        },
-        {
-            path: '/signup',
-            element: <SignUp></SignUp>
-        },
-        {
-          path: '/mytoys',
-          element: <PrivateRoute><MyToys></MyToys></PrivateRoute>
-        },
-        {
-          path: '/details/:id',
-          element: <PrivateRoute><Details></Details></PrivateRoute>,
-          loader: ({params}) => fetch(`https://toy-marketplace-server-henna.vercel.app/toys/${params.id}`)
-        },
-        {
-          path: '/edit/:id',
-          element: <EditToy></EditToy>,
-          loader: ({params}) => fetch(`https://toy-marketplace-server-henna.vercel.app/toys/${params.id}`)
-        },
-        {
-          path: '/alltoys',
-          element: <AllToy></AllToy>,
-          loader: () => fetch('https://toy-marketplace-server-henna.vercel.app/toys')
-        },
-        {
-          path: '/addtoy',
-          element: <PrivateRoute><AddToy></AddToy></PrivateRoute>
-        },
-        {
-          path: '/blog',
-          element: <Blog></Blog>
-        }
-      ]
-    },
-    {
-      path: "*",
-      element: <ErrorPage></ErrorPage>
-    }
-  ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <AuthProvider>
+        <Main></Main>
+      </AuthProvider>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/signup",
+        element: <SignUp></SignUp>,
+      },
+      {
+        path: "/mytoys",
+        element: (
+          <PrivateRoute>
+            <MyToys></MyToys>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/details/:id",
+        element: (
+          <PrivateRoute>
+            <Details></Details>
+          </PrivateRoute>
+        ),
+        loader: ({ params }) =>
+          fetch(
+            `https://toy-marketplace-server-henna.vercel.app/toys/${params.id}`
+          ),
+      },
+      {
+        path: "/edit/:id",
+        element: <EditToy></EditToy>,
+        loader: ({ params }) =>
+          fetch(
+            `https://toy-marketplace-server-henna.vercel.app/toys/${params.id}`
+          ),
+      },
+      {
+        path: "/alltoys",
+        element: <AllToy></AllToy>,
+        loader: () =>
+          fetch("https://toy-marketplace-server-henna.vercel.app/toys"),
+      },
+      {
+        path: "/addtoy",
+        element: (
+          <PrivateRoute>
+            <AddToy></AddToy>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <ErrorPage></ErrorPage>,
+  },
+]);
 
 export default router;
